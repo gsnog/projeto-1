91b95be79e048c92879aab1518de212b2a3f7c33
rafaelcuns <rafaplayrj2@gmail.com>
2025-02-10 18:10:16 -0300

    JS feito adicionar-lobinho

--- adicionar-lobinho.js
+++ adicionar-lobinho.js
@@ -1,12 +1,17 @@
 let lobos = JSON.parse(localStorage.getItem('lobos'))
-document.querySelector("button").addEventListener("click", () => {
-    lobos.push({
-        id: lobos.length + 1,
-        nome: document.querySelector("input[name='nome']").value,
-        idade: document.querySelector("input[name='idade']").value,
-        descricao: document.querySelector("textarea").value,
-        imagem: document.querySelector("input[name='foto']").value,
-        adotado: false
-    })
-    localStorage.setItem('lobos', JSON.stringify(lobos))
+document.querySelector("div.adicionar-lobo button").addEventListener("click", () => {
+    if (document.querySelector("input[name='nome']").value == "" || document.querySelector("input[name='idade']").value == "" || document.querySelector("textarea").value == "" || document.querySelector("input[name='link-da-foto']").value == "") {
+        window.alert("Dados incompletos")
+    } else {
+        lobos.push({
+            id: lobos.length + 1,
+            nome: document.querySelector("input[name='nome']").value,
+            idade: document.querySelector("input[name='idade']").value,
+            descricao: document.querySelector("textarea").value,
+            imagem: document.querySelector("input[name='link-da-foto']").value,
+            adotado: false
+        })
+        localStorage.setItem('lobos', JSON.stringify(lobos))
+        window.location.href = 'index.html'
+    }
 })
