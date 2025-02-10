898a0deed06f12d0b5722433f653c802d3bb2a46
rafaelcuns <rafaplayrj2@gmail.com>
2025-02-10 17:51:22 -0300

    Adicionando inicialização do localstorage na home

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+async function inicializarLocalStorage() {
+    try {
+        const response = await fetch('lobinhos.json');
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar lobinho.json: ${response.statusText}`);
+        }
+        const lobos = await response.json();
+        localStorage.setItem('lobos', JSON.stringify(lobos));
+        console.log('Lobos inicializados no localStorage');
+    } catch (error) {
+        console.error('Erro ao inicializar o localStorage:', error);
+    } finally {
+        console.log('Tentativa de inicialização do localStorage concluída');
+    }
+}
+
+if (!localStorage.getItem('lobos')) {
+    inicializarLocalStorage().then(() => {
+        console.log('Inicialização do localStorage concluída');
+    }).catch(error => {
+        console.error('Erro durante a inicialização do localStorage:', error);
+    });
+}
+
 let lobos = JSON.parse(localStorage.getItem('lobos'))
 
 document.querySelector('div.exemplo1 div h4').innerHTML = lobos[0].nome
