fb226e4c66b3c2837f7a498dca9b00de33a6cefc
rafaelcuns <rafaplayrj2@gmail.com>
2025-02-11 12:29:17 -0300

    JS feito show-lobinho

--- show-lobinho.js
+++ show-lobinho.js
@@ -4,15 +4,23 @@ const id = urlParams.get('id')
 let lobos = JSON.parse(localStorage.getItem('lobos'))
 lobos.forEach(lobo => {
     if (lobo.id == id) {
-        document.querySelector("h1").innerHTML = lobo.nome
+        document.querySelector(".titulo h2").innerHTML = lobo.nome
+        const p = document.createElement('p')
+        p.innerHTML = lobo.descricao
+        document.querySelector(".textos").innerHTML = ""
+        document.querySelector(".textos").append(p)
+        if (!lobo.adotado) {
+            document.querySelector(".botao1").style.display = "block"
+        }
     }
 })
 
 function adotar() {
-    window.location.href = `testeAdotar.html?id=${encodeURIComponent(id)}`
+    window.location.href = `adotar-lobinho.html?id=${encodeURIComponent(id)}`
 }
 
 function excluir() {
     novosLobos = lobos.filter(lobo => lobo.id != id)
     localStorage.setItem('lobos', JSON.stringify(novosLobos))
+    window.location.href = "lista-de-lobinhos.html"
 }
